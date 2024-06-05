031e3e48234f29b32bed888e45c55a00e67e561d
galKatzav <galoosh1591@gmail.com>
2024-06-05 18:19:38 +0300

    Page404 Update

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Page404.css";
 
 const Page404 = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h2>Page not found</h2>
+    <div className="page404-container">
+      <h1>Oops! Page not found</h1>
+      <p>We can't seem to find the page you're looking for.</p>
+      <div className="graphic-container">
+        <div className="graphic-404">
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+      </div>
+      <button onClick={handleGoHome} className="home-button">
+        Go to Home
+      </button>
     </div>
   );
 };
