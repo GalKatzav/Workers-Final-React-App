86c94f9435e36196656731f1d63745fb0e483bfa
galKatzav <galoosh1591@gmail.com>
2024-06-09 11:43:49 +0300

    Search Update

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
 import { EmployeeContext } from "../context/EmployeeContext";
 import EmployeeCard from "../components/EmployeeCard";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "../style/SearchResults.css"; // צור קובץ CSS לעיצוב העמוד במידת הצורך
 
 const SearchResults = () => {
   const { searchTerm } = useParams();
   const { allEmployees, fetchEmployees } = useContext(EmployeeContext);
   const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [newSearchTerm, setNewSearchTerm] = useState(searchTerm);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchEmployees(searchTerm);
@@ -18,17 +21,32 @@ const SearchResults = () => {
     }
   }, [allEmployees]);
 
+  const handleSearch = () => {
+    navigate(`/search/${newSearchTerm}`);
+  };
+
   return (
-    <div className="employee-list">
-      {filteredEmployees.map((emp, index) => (
-        <EmployeeCard
-          key={emp.email}
-          employee={emp}
-          delay={index * 100}
-          showDetails={true}
-          index={index}
+    <div className="search-results">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search by company"
+          value={newSearchTerm}
+          onChange={(e) => setNewSearchTerm(e.target.value)}
         />
-      ))}
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="employee-list">
+        {filteredEmployees.map((emp, index) => (
+          <EmployeeCard
+            key={emp.email}
+            employee={emp}
+            delay={index * 100}
+            showDetails={true}
+            index={index}
+          />
+        ))}
+      </div>
     </div>
   );
 };
